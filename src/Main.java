bdda13c6b03afc2ec0fcd9cc57032f36bdb973cc
Somesh Tiwari <someshtiwari96@gmail.com>
2026-07-20 21:46:15 +0530

    first program

---
+++ src/Main.java
@@ -0,0 +1,11 @@
+import java.lang.*;
+
+public class Main {
+    public static void sum(int a, int b) {
+        int add = a + b;
+        System.out.println(add);
+    }
+    public static void main(String[] args) {
+        sum(5, 3);
+    }
+}
