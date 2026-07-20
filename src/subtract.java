999dc16e4969cb0c08ea1bbd9270a4bc53fe9768
Somesh Tiwari <someshtiwari96@gmail.com>
2026-07-20 21:44:28 +0530

    first program

---
+++ src/subtract.java
@@ -0,0 +1,11 @@
+public class subtract {
+
+    public static void main(String[] args){
+        subtract(8,1);
+    }
+
+    public static void subtract(int a, int b){
+        int sub = b-a;
+        System.out.println(sub);
+    }
+}
